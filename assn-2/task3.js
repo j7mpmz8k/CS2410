7dc0308bcdf5be1b866ac5238a124f5ecdbf81c4
Jacob Cardon <j7mpmz8k@duck.com>
2026-05-15 18:22:47 -0500

    minor typos

--- assn-2/task3.js
+++ assn-2/task3.js
@@ -1,24 +1,24 @@
 //default values
-const PRINCIPLE = 300000;
+const PRINCIPAL = 300000;
 const RATE = 5.5;
 const YEARS = 30;
 
-const priciple = document.getElementById("priciple");
+const principal = document.getElementById("principal");
 const rate = document.getElementById("rate");
 const years = document.getElementById("years");
 const output = document.getElementById("output");
 
-priciple.value = PRINCIPLE;
+principal.value = PRINCIPAL;
 rate.value = RATE;
 years.value = YEARS;
 output.innerHTML = calculatePayments();
 
 function calculatePayments() {
     //get total loan amount (P)
-    if (!priciple.value) {
-        priciple.value = PRINCIPLE;
+    if (!principal.value) {
+        principal.value = PRINCIPAL;
     }
-    const P = parseFloat(priciple.value);
+    const P = parseFloat(principal.value);
     if (Number.isNaN(P)) {
         return "Loan amount entered is not a number!"
     }
@@ -31,43 +31,38 @@ function calculatePayments() {
     }
     const r = parseFloat(rate.value) / 1200;//converts annual % to monthy rate as decimal
     if (Number.isNaN(r)) {
-        return "Annunl Rate entered is not a number!"
-    }
-    if (r < 0) {
-        return "Annual Rate cannot be negative!"
+        return "Annual Rate entered is not a number!"
     }
     // get number of months (n)
     if (!years.value) {
         years.value = YEARS;
     }
-    const n = parseFloat(years.value) * 12;
-    if (n <= 0) {
-        return "Number of Years must be greater than zero!"
-    }
-    if (n < 1 && n > 0) {
-        n = 1;
-    }
+    let n = parseFloat(years.value) * 12;
     if (Number.isNaN(n)) {
         return "Number of Years entered is not a number!"
     }
+    if (n <= 0) {
+        return "Number of Years must be greater than zero!"
+    }
 
+    if (n < 1 && n > 0) {
+        n = 1;//most not be less than a month or else it incorrectly inflates the monthly payments
+    }
+    //calculates the monthly payments (M)
     let M;
     if (Math.abs(r) < 1e-8) {
-        M = P / n;
+        M = P / n;//evenly devides if there is no interest rate
     } else {
-        M = P * ((r * ((1 + r) ** n)) / (((1 + r) ** n) - 1));
+        M = P * ((r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1));
     }
     M = Math.round(M * 100) / 100;// rounds to nearest hundredth
     if (Number.isNaN(M)) {
-        return false;
-    }
-    if (M > P) {
-        M = P;
+        return false;//needed in cases of too large entered numbers
     }
     return `Monthly Payment: $${M}`;
 }
 
-priciple.addEventListener("blur", () => {
+principal.addEventListener("blur", () => {
     let msg = calculatePayments();
     if (!msg) {
         msg = "Loan amount entered is too large to calculate!"
@@ -77,7 +72,7 @@ priciple.addEventListener("blur", () => {
 rate.addEventListener("blur", () => {
     let msg = calculatePayments();
     if (!msg) {
-        msg = "Annunl Rate entered is too large to calculate!"
+        msg = "Annual Rate entered is too large to calculate!"
     }
     output.innerHTML = msg;
 });
