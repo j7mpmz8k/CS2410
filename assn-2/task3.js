2c561fd0a4281b4b7620c3d1250a0327853831c9
Jacob Cardon <j7mpmz8k@duck.com>
2026-05-15 18:27:33 -0500

    minor formatting for comments

--- assn-2/task3.js
+++ assn-2/task3.js
@@ -1,4 +1,4 @@
-//default values
+// default values
 const PRINCIPAL = 300000;
 const RATE = 5.5;
 const YEARS = 30;
@@ -14,7 +14,7 @@ years.value = YEARS;
 output.innerHTML = calculatePayments();
 
 function calculatePayments() {
-    //get total loan amount (P)
+    // get total loan amount (P)
     if (!principal.value) {
         principal.value = PRINCIPAL;
     }
@@ -25,14 +25,16 @@ function calculatePayments() {
     if (P <= 0) {
         return "Loan amount must be greater than zero!"
     }
-    //get monthy rate (r)
+
+    // get monthy rate (r)
     if (!rate.value) {
         rate.value = RATE;
     }
-    const r = parseFloat(rate.value) / 1200;//converts annual % to monthy rate as decimal
+    const r = parseFloat(rate.value) / 1200; // converts annual % to monthy rate as decimal
     if (Number.isNaN(r)) {
         return "Annual Rate entered is not a number!"
     }
+
     // get number of months (n)
     if (!years.value) {
         years.value = YEARS;
@@ -46,19 +48,22 @@ function calculatePayments() {
     }
 
     if (n < 1 && n > 0) {
-        n = 1;//most not be less than a month or else it incorrectly inflates the monthly payments
+        n = 1; // most not be less than a month or else it incorrectly inflates the monthly payments
     }
-    //calculates the monthly payments (M)
+
+    // calculates the monthly payments (M)
     let M;
     if (Math.abs(r) < 1e-8) {
-        M = P / n;//evenly devides if there is no interest rate
+        M = P / n; // evenly devides if there is no interest rate
     } else {
         M = P * ((r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1));
     }
-    M = Math.round(M * 100) / 100;// rounds to nearest hundredth
+    M = Math.round(M * 100) / 100; // rounds to nearest hundredth
+    
     if (Number.isNaN(M)) {
-        return false;//needed in cases of too large entered numbers
+        return false; // needed in cases of too large entered numbers
     }
+    
     return `Monthly Payment: $${M}`;
 }
 
